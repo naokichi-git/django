64727c2f6855ec4b59e9b403c62d25813645f2b2
NaokiHashizume <50830297+NaokiHashizume@users.noreply.github.com>
2020-04-15 23:40:21 +0900

    successful

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,9 +105,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+
+TIME_ZONE = 'Asia/Tokyo'
+
+# LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
